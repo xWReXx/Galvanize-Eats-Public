23a41d28492237e275ca2276ed2e17564568e318
xwrexx <wrex1285@gmail.com>
2018-09-27 13:45:45 -0600

    added buttons to add items from the menu to check out

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 console.log("app.js linked")
 
-
 var tableMenu = document.getElementById('menuTable')
-
 
 fetch('https://galvanize-eats-api.herokuapp.com/menu')
     .then((response) => response.json())
@@ -13,14 +11,21 @@ fetch('https://galvanize-eats-api.herokuapp.com/menu')
             var tablePrice = document.createElement('td')
             var tableType = document.createElement('td')
             var tableName = document.createElement('td')
+            var atag = document.createElement('a')
+            var itag = document.createElement('i')
+
             tableMenu.appendChild(tableRow)
             tableRow.appendChild(tablePrice)
-            tablePrice.innerHTML = data.menu[i].price
+            tablePrice.innerHTML = "$" + data.menu[i].price
             tableRow.appendChild(tableType)
             tableType.innerHTML = data.menu[i].type
             tableRow.appendChild(tableName)
             tableName.innerHTML = data.menu[i].name
-
+            tableRow.appendChild(atag)
+            atag.appendChild(itag)
+            atag.setAttribute('class','btn-floating btn-medium waves-effect waves-light amber darken-4 addButton')
+            itag.setAttribute('class','material-icons')
+            itag.innerHTML = 'add'
         }
     })
     .catch(console.error)
