9578ba48ee7e49eebb256bd50f51f6ce5761259a
xwrexx <wrex1285@gmail.com>
2018-10-04 03:57:15 -0600

    prices fixed to two decimal places, remove itme button now works

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var cancelArr = []
 var order = {}
 var price = 0
 var totalPrice = 0
+var afterTax = 0
+var fixedPrice = totalPrice.toFixed(2)
 
 fetch('https://galvanize-eats-api.herokuapp.com/menu')
     .then((response) => response.json())
@@ -17,7 +19,6 @@ fetch('https://galvanize-eats-api.herokuapp.com/menu')
             var tableName = document.createElement('td')
             var atag = document.createElement('a')
             var itag = document.createElement('i')
-
             tableMenu.appendChild(tableRow)
             tableRow.appendChild(tablePrice)
             tablePrice.innerHTML = "$" + data.menu[i].price
@@ -37,43 +38,35 @@ fetch('https://galvanize-eats-api.herokuapp.com/menu')
         var buttonArr = document.getElementsByClassName('addToOrder')
         var tableCheckout = document.getElementById('checkoutTable')
         var subTotalPrice = document.getElementById('subTotal')
-
+        var finalPrice = document.getElementById('finalPrice')
         for (let i = 0; i < buttonArr.length; i++) {
             buttonArr[i].addEventListener("click", function (event) {
                 var name = event.target.getAttribute('data-name')
-                
                 if (order[name]) {
                     order[name].quantity++
-                    order[name].price += Number(event.target.getAttribute('data-price'))
+                    order[name].subTotal += Number(event.target.getAttribute('data-price'))
                     totalPrice += Number(event.target.getAttribute('data-price'))
-                    console.log(totalPrice)
+                    afterTax = (totalPrice * 0.08) + totalPrice
                 } else {
                     order[name] = {
                         quantity: 1,
-                        price: Number(event.target.getAttribute('data-price'))
+                        price: Number(event.target.getAttribute('data-price')),
+                        subTotal: Number(event.target.getAttribute('data-price')),
+                        name: event.target.getAttribute('data-name')
                     }
                     totalPrice += Number(event.target.getAttribute('data-price'))
+                    afterTax = (totalPrice * 0.08) + totalPrice
                 }
-                subTotalPrice.innerHTML = "Sub Total: $" + totalPrice
+
+                subTotalPrice.innerHTML = "Sub Total: $" + totalPrice.toFixed(2)
+                finalPrice.innerHTML = "After Tax $" + afterTax.toFixed(2)
                 generateTable()
-                getCancelButton()
             })
+
+
+
         }
-        console.log(cancelArr.length)
-        for (let i = 0; i < cancelArr.length; i++) {
-            cancelArr[i].addEventListener("click", function (event) {
-                var name = event.target.getAttribute('data-name')
-                if (order[name].price > "0") {
-                    order[name].quantity--
-                    order[name].price -= Number(event.target.getAttribute('data-price'))
-                }
-                generateTable()
-                
-            })
-        }
-        function getCancelButton(){
-            cancelArr = document.getElementsByClassName('cancelItem')
-        }
+
 
         function generateTable() {
             tableCheckout.innerHTML = ""
@@ -92,9 +85,9 @@ fetch('https://galvanize-eats-api.herokuapp.com/menu')
             itemTH.innerHTML = "Item"
             qunatityTH.innerHTML = "Quantity"
             cancelTH.innerHTML = "Remove Item"
-            
+
             for (var item in order) {
-                console.log(item)
+                var fixedSubTotal = order[item].subTotal
                 var checkoutTR = document.createElement('tr')
                 var priceTD = document.createElement('td')
                 var nameTD = document.createElement('td')
@@ -103,7 +96,7 @@ fetch('https://galvanize-eats-api.herokuapp.com/menu')
                 var itag1 = document.createElement('i')
                 tableCheckout.appendChild(checkoutTR)
                 checkoutTR.appendChild(priceTD)
-                priceTD.innerHTML = "$" + order[item].price
+                priceTD.innerHTML = "$" + fixedSubTotal.toFixed(2)
                 checkoutTR.appendChild(nameTD)
                 nameTD.innerHTML = item
                 checkoutTR.appendChild(quantityTD)
@@ -111,12 +104,27 @@ fetch('https://galvanize-eats-api.herokuapp.com/menu')
                 checkoutTR.appendChild(atag1)
                 atag1.appendChild(itag1)
                 atag1.setAttribute('class', 'btn-floating btn-medium waves-effect waves-light amber darken-4')
-                itag1.setAttribute('class', 'material-icons cancelItem')
+                itag1.setAttribute('class', 'material-icons cancelButton')
                 itag1.setAttribute('data-price', order[item].price)
+                itag1.setAttribute('data-totalPrice', order[item].subTotal)
                 itag1.setAttribute('data-name', order[item].name)
                 itag1.innerHTML = 'cancel'
-                console.log(item)
-                console.log(order[item].name)
+
+            }
+            cancelArr = document.getElementsByClassName('cancelButton')
+            for (var i = 0; i < cancelArr.length; i++) {
+                cancelArr[i].addEventListener("click", function (event) {
+                    var name = event.target.getAttribute('data-name')
+                    if (Number(order[name].subTotal) > 0) {
+                        order[name].quantity--
+                        order[name].subTotal = Number(event.target.getAttribute('data-totalPrice')) - Number(event.target.getAttribute('data-price'))
+                        totalPrice -= Number(event.target.getAttribute('data-price'))
+                        afterTax = (totalPrice * 0.08) + totalPrice
+                        subTotalPrice.innerHTML = "Sub Total: $" + totalPrice.toFixed(2)
+                        finalPrice.innerHTML = "After Tax $" + afterTax.toFixed(2)
+                        generateTable()
+                    }
+                })
             }
         }
     })
